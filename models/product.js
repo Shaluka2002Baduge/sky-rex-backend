d18eb31c550de2636b56f64c2e0c5e14901bb5f2
Shaluka2002Baduge <kbsdulanjana66@gmail.com>
2026-08-10 21:13:09 +0530

    Day-16

--- models/product.js
+++ models/product.js
@@ -37,6 +37,12 @@ const productSchema = new mongoose.Schema(
         category : {
              type : String,
              required : true 
+    },
+    stock: {
+        type : Number,
+        required: true,
+        default: 0
+
     }
 }
 )
